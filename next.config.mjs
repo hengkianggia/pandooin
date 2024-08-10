028e6558a3eaca021dfb018460f32f314191b02f
HengkiAnggia <pengkyjd00@gmail.com>
2024-08-10 14:53:06 +0700

    chore: Update image domains in next.config.mjs
    - Add "images.unsplash.com" to the list of image domains in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["ik.imagekit.io", "images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 };
 
